6bf45bc27424df3f1a341fd96593eee3071a7bd3
Dmitry <pashula.dmitro@lanars.com>
2021-10-30 19:45:05 +0300

    user logic

---
+++ src/components /UserList.tsx
@@ -0,0 +1,29 @@
+import React, {useEffect} from 'react';
+import {useTypedSelector} from '../hooks/useTypedSelector';
+import {useActions} from '../hooks/useActions';
+
+const UserList: React.FC = () => {
+    const { users, error, loading } = useTypedSelector((state) => state.userReducer);
+  const {fetchUsers} = useActions();
+
+
+    useEffect(()=> {
+        fetchUsers();
+    }, [])
+
+    if(loading) {
+        return <div> Идёт загрузка, подождите</div>
+    }
+    if(error) {
+        return <div>{error}</div>
+    }
+    return (
+        <div>
+            {
+                users.map(user => <div key={user.id}>{user.name}</div>)
+            }
+        </div>
+    );
+};
+
+export default UserList;
